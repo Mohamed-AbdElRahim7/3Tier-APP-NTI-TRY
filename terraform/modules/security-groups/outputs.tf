6c819a4e74e82a5598d3d5e575c08dd6451d9dbd
Mohamed Abd El_Rahim <161028740+Mohamed-AbdElRahim7@users.noreply.github.com>
2025-06-14 06:28:45 +0300

    Delete terraform directory

--- terraform/modules/security-groups/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "rds_sg_id" {
-  value       = aws_security_group.rds_sg.id
-  description = "RDS security group ID"
-}
-
-output "jenkins_sg_id" {
-  value       = aws_security_group.jenkins_sg.id
-  description = "Jenkins EC2 security group ID"
-}
