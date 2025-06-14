8151cc152fb08411d17ce42b305dc30dc04b6da7
Mohamed Abd El_Rahim <161028740+Mohamed-AbdElRahim7@users.noreply.github.com>
2025-06-14 09:05:41 +0300

    Add files via upload

---
+++ terraform/modules/security-groups/variables.tf
@@ -0,0 +1,22 @@
+variable "project_name" {
+  type        = string
+  description = "Project name for tagging"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID to assign security groups"
+}
+
+variable "eks_nodes_cidr" {
+  type        = string
+  description = "CIDR block for EKS nodes"
+}
+variable "vpc_id" {
+  type = string
+  description = "VPC ID to associate the SG"
+}
+variable "eks_node_sg_id" {
+  type = string
+  description = "Security Group ID of EKS nodes"
+}
