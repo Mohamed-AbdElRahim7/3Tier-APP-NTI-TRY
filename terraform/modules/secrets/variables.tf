419815ce6ac4d620adf070e4fd114d25eb53ef42
Mohamed Abd El_Rahim <161028740+Mohamed-AbdElRahim7@users.noreply.github.com>
2025-06-14 07:24:03 +0300

    Add files via upload

---
+++ terraform/modules/secrets/variables.tf
@@ -0,0 +1,17 @@
+
+variable "db_username" {
+  description = "Database master username"
+  type        = string
+}
+
+variable "db_password" {
+  description = "Database master password"
+  type        = string
+}
+
+  type = string
+  description = "RDS Username"
+}
+  type = string
+  description = "RDS Password"
+}
