831bb64116c0e45481682e0e15d056d8623785a3
Mohamed Abd El_Rahim <161028740+Mohamed-AbdElRahim7@users.noreply.github.com>
2025-06-14 16:20:49 +0300

    Add files via upload

---
+++ terraform/modules/rds/variables.tf
@@ -0,0 +1,49 @@
+variable "project_name" {
+  type        = string
+  description = "Project name for tagging"
+}
+
+variable "private_subnet_ids" {
+  type        = list(string)
+  description = "Private subnet IDs for RDS subnet group"
+}
+
+variable "db_name" {
+  type        = string
+  description = "Database name"
+}
+
+variable "db_instance_class" {
+  type        = string
+  description = "Instance class for RDS"
+}
+
+variable "db_allocated_storage" {
+  type        = number
+  description = "Allocated storage for RDS"
+}
+
+variable "db_sg_id" {
+  type        = string
+  description = "Security Group ID for RDS instance"
+}
+
+variable "db_secret_arn" {
+  type        = string
+  description = "ARN of the secret storing RDS credentials"
+}
+
+
+
+
+
+variable "db_username" {
+  type = string
+  description = "Username for RDS"
+}
+
+
+variable "db_password" {
+  type = string
+  description = "Password for RDS"
+}
