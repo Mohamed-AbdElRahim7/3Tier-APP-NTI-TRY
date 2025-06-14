2edbba38a6bb87f1a4b2217e70eea222dfa278a4
Mohamed Abd El_Rahim <161028740+Mohamed-AbdElRahim7@users.noreply.github.com>
2025-06-14 03:43:17 +0300

    Add files via upload

---
+++ terraform/modules/rds/main.tf
@@ -0,0 +1,30 @@
+data "aws_secretsmanager_secret_version" "rds_secret" {
+  secret_id = var.db_secret_arn
+}
+
+locals {
+  rds_credentials = jsondecode(data.aws_secretsmanager_secret_version.rds_secret.secret_string)
+}
+
+resource "aws_db_subnet_group" "rds_subnet_group" {
+  subnet_ids = var.private_subnet_ids
+
+  tags = {
+  }
+}
+
+resource "aws_db_instance" "main" {
+  allocated_storage       = var.db_allocated_storage
+  engine                  = "mysql"
+  engine_version          = "8.0"
+  instance_class          = var.db_instance_class
+  password                = local.rds_credentials.password
+  vpc_security_group_ids  = [var.db_sg_id]
+  skip_final_snapshot     = true
+  publicly_accessible     = false
+  multi_az                = false
+  storage_encrypted       = true
+
+  tags = {
+  }
+}
