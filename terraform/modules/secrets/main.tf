19b5c3b702a8ea2bed1305ae398e255d66ea0ac5
Mohamed Abd El_Rahim <161028740+Mohamed-AbdElRahim7@users.noreply.github.com>
2025-06-14 17:28:54 +0300

    Add files via upload

---
+++ terraform/modules/secrets/main.tf
@@ -0,0 +1,16 @@
+resource "aws_secretsmanager_secret" "rds" {
+  name = "${var.project_name}-rds-credentials-33"
+
+  tags = {
+    Name    = "${var.project_name}-rds-secret"
+    Project = var.project_name
+  }
+}
+
+resource "aws_secretsmanager_secret_version" "rds" {
+  secret_id     = aws_secretsmanager_secret.rds.id
+  secret_string = jsonencode({
+
+
+  })
+}
