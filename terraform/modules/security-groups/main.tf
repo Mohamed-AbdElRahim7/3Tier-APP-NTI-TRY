6193c748323965060a341eb8e8b7b3e92aad2f42
Mohamed Abd El_Rahim <161028740+Mohamed-AbdElRahim7@users.noreply.github.com>
2025-06-14 09:21:14 +0300

    Update main.tf

--- terraform/modules/security-groups/main.tf
+++ terraform/modules/security-groups/main.tf
@@ -1,12 +1,14 @@
+resource "aws_security_group" "rds_sg" {
+  name        = "${var.project_name}-rds-sg"
   description = "Allow MySQL from EKS Nodes"
   vpc_id      = var.vpc_id
 
   ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    description = "Allow MySQL from EKS Nodes"
-    cidr_blocks = [var.eks_nodes_cidr]
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [var.eks_node_sg_id]
+    description     = "Allow MySQL from EKS Nodes"
   }
 
   egress {
@@ -51,49 +53,5 @@ resource "aws_security_group" "jenkins_sg" {
 
   tags = {
     Name = "${var.project_name}-jenkins-sg"
-  }
-}
-
-  vpc_id = var.vpc_id
-
-  ingress {
-    from_port       = 3306
-    to_port         = 3306
-    protocol        = "tcp"
-    security_groups = [var.eks_node_sg_id]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "${var.project_name}-rds-sg"
-  }
-}
-resource "aws_security_group" "rds_sg" {
-  name        = "${var.project_name}-rds-sg"
-  description = "Allow MySQL from EKS Nodes"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    from_port       = 3306
-    to_port         = 3306
-    protocol        = "tcp"
-    security_groups = [var.eks_node_sg_id]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "${var.project_name}-rds-sg"
   }
 }
