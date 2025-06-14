4d8d769e26c16022b61ff326a816d55ae9737a3b
Mohamed Abd El_Rahim <161028740+Mohamed-AbdElRahim7@users.noreply.github.com>
2025-06-14 03:41:07 +0300

    Delete terraform directory

--- terraform/modules/rds/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "rds_endpoint" {
-  value       = aws_db_instance.main.endpoint
-  description = "RDS endpoint for connecting from backend"
-}
-
-output "rds_identifier" {
-  value       = aws_db_instance.main.id
-  description = "RDS instance identifier"
-}
