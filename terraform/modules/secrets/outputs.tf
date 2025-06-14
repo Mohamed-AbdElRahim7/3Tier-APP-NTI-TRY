6b713a9e498e146c872ff981f92df8de23c51800
Mohamed Abd El_Rahim <161028740+Mohamed-AbdElRahim7@users.noreply.github.com>
2025-06-14 06:43:48 +0300

    Update outputs.tf

--- terraform/modules/secrets/outputs.tf
+++ terraform/modules/secrets/outputs.tf
@@ -7,6 +7,3 @@ output "secret_name" {
   value       = aws_secretsmanager_secret.rds.name
   description = "Name of the secret"
 }
-output "secret_name" {
-  value = aws_secretsmanager_secret.rds.name
-}
