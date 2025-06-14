212086b2ca3cbf4fc13f48fde2e5534976495475
Mohamed Abd El_Rahim <161028740+Mohamed-AbdElRahim7@users.noreply.github.com>
2025-06-14 05:40:14 +0300

    Update main.tf

--- terraform/modules/secrets/main.tf
+++ terraform/modules/secrets/main.tf
@@ -1,5 +1,5 @@
 resource "aws_secretsmanager_secret" "rds" {
-  name = "${var.project_name}-rds-credentials-28"
+  name = "${var.project_name}-rds-credentials-V1"
 
   tags = {
     Name    = "${var.project_name}-rds-secret"
