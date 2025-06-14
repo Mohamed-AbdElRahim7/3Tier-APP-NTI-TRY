2200e12312a9ac1f8ff814cc9d3ce3c14d6b202a
Mohamed Abd El_Rahim <161028740+Mohamed-AbdElRahim7@users.noreply.github.com>
2025-06-14 07:44:11 +0300

    Delete terraform directory

--- terraform/modules/backup/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "backup_vault_name" {
-  value       = aws_backup_vault.jenkins_vault.name
-  description = "Name of the backup vault"
-}
-
-output "backup_plan_id" {
-  value       = aws_backup_plan.jenkins_plan.id
-  description = "ID of the backup plan"
-}
