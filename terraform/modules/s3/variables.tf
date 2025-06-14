e771baade2e9eb3617cdee742d45b94bfa9e80a8
Mohamed Abd El_Rahim <161028740+Mohamed-AbdElRahim7@users.noreply.github.com>
2025-06-14 08:07:26 +0300

    Add files via upload

---
+++ terraform/modules/s3/variables.tf
@@ -0,0 +1,9 @@
+variable "project_name" {
+  type        = string
+  description = "Project name for tagging"
+}
+
+variable "bucket_name" {
+  type        = string
+  description = "Name of the S3 bucket"
+}
