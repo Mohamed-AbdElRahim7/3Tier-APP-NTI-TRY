60e3a00b6420c8ca3537278bfee9fdc4d862bf10
Mohamed Abd El_Rahim <161028740+Mohamed-AbdElRahim7@users.noreply.github.com>
2025-06-14 07:04:51 +0300

    Update variables.tf

--- terraform/modules/rds/variables.tf
+++ terraform/modules/rds/variables.tf
@@ -33,3 +33,9 @@ variable "db_secret_arn" {
   description = "ARN of the secret storing RDS credentials"
 }
 
+variable "db_username" {
+  description = "Database master username"
+  type        = string
+}
+
+
